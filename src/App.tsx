4157902a630a65d8b09c6160e089a6d94f1ab328
Maxim <max766667@gmail.com>
2021-09-29 11:28:08 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactttt
         </a>
       </header>
     </div>
