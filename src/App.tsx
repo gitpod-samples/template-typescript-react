3df884be775773b1ac3cd78823b9fb1abeb1fe0c
d3rox <notopograne@gmail.com>
2021-12-22 03:02:18 +0000

    logo change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          d3vLAB Tool
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          one tool to boot your root
         </a>
       </header>
     </div>
