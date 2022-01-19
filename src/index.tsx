0d21b40db358930001bdd819468b0e8ca5353813
Sven Efftinge <sven@gitpod.io>
2022-01-19 09:37:24 +0000

    failed prebuild

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')<yw
 );
 
 // If you want to start measuring performance in your app, pass a function
