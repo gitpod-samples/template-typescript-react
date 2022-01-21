b2b5b9583513c982e4c88b8770d76111ef28b8b0
Sven Efftinge <sven.efftinge@typefox.io>
2022-01-21 09:15:34 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')<ywd
+  document.getElementById('root')<ywdssd
 );
 
 // If you want to start measuring performance in your app, pass a function
