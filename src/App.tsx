534cf5355e48b0c58a11392872685bd86e74f847
epinnock <epinnock@gmail.com>
2022-09-16 03:43:51 +0000

    learning all the reacts

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacts
         </a>
       </header>
     </div>
