513ea1057060f80704798be85163d6c8e96554cf
Avadhesh Singh <avadheshsingh9634@gmail.com>
2023-04-18 14:32:46 +0000

    code update from git

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleNum1Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNum1(event.target.value);
+  };
+
+  const handleNum2Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNum2(event.target.value);
+  };
+
+  const handleAddition = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const sum = parseInt(num1) + parseInt(num2);
+    setResult(sum.toString());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleAddition}>
+      <label>
+       Enter the First Number :
+        <input type="number" value={num1} onChange={handleNum1Change} />
+      </label>
+      <br />
+      <label>
+      Enter the Second Number :
+        <input type="number" value={num2} onChange={handleNum2Change} />
+      </label>
+      <br/>
+      <label>
+       Result :
+        <input type="text" value={result} readOnly />
+      </label>
+      <br />
+      <button type="submit">Sum</button>
+    </form>
   );
-}
+};
 
 export default App;
+
+
